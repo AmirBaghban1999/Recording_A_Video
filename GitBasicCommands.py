416631fc0edff89cb95e3dcc95f7c54459d2c10a
Amir Baghban <amirbaghban@aut.ac.ir>
2023-04-11 12:53:11 +0430

    Revert "Options"
    This reverts commit 16a0a1b3762409366cdaa7b4e44ed8fdf18e3179.

--- GitBasicCommands.py
+++ GitBasicCommands.py
@@ -5,10 +5,3 @@ if answer=='Yes':
 else:
     print('You can think about going out')
 
-answer1=str(input('Are you pleased with the situation?'))
-
-if answer1=='Yes':
-    print('go on with your plan')
-
-else:
-    answer2 = str(input('What would you like to do? Sports or going out'))
